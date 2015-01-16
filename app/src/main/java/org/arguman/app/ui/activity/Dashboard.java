026188c527a29d941952593f9941c1a53aafd3c5
tokiyashi <tokiyashi@gmail.com>
2015-01-17 00:27:22 +0200

    Moved SamplePagerAdapter to DashboardPagerAdapter.

--- app/src/main/java/org/arguman/app/ui/activity/Dashboard.java
+++ app/src/main/java/org/arguman/app/ui/activity/Dashboard.java
@@ -1,16 +1,13 @@
 package org.arguman.app.ui.activity;
 
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import org.arguman.app.R;
+import org.arguman.app.ui.adapter.DashboardPagerAdapter;
 import org.arguman.app.ui.view.SlidingTabLayout;
 
 public class Dashboard extends ActionBarActivity {
@@ -23,7 +20,7 @@ public class Dashboard extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new SamplePagerAdapter());
+        viewPager.setAdapter(new DashboardPagerAdapter(this));
         slidingTabLayout = (SlidingTabLayout) findViewById(R.id.tab_layout);
         slidingTabLayout.setViewPager(viewPager);
     }
@@ -39,73 +36,4 @@ public class Dashboard extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    class SamplePagerAdapter extends PagerAdapter {
-
-        /**
-         * @return the number of pages to display
-         */
-        @Override
-        public int getCount() {
-            return 5;
-        }
-
-        /**
-         * @return true if the value returned from
-         * {@link #instantiateItem(ViewGroup, int)} is the same object
-         * as the {@link View} added to the {@link ViewPager}.
-         */
-        @Override
-        public boolean isViewFromObject(View view, Object o) {
-            return o == view;
-        }
-
-        // BEGIN_INCLUDE (pageradapter_getpagetitle)
-
-        /**
-         * Return the title of the item at {@code position}. This is important
-         * as what this method returns is what is displayed in the
-         * {@link SlidingTabLayout}.
-         * <p/>
-         * Here we construct one using the position value, but for real
-         * application the title should refer to the item's contents.
-         */
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "Item " + (position + 1);
-        }
-
-        // END_INCLUDE (pageradapter_getpagetitle)
-
-        /**
-         * Instantiate the {@link View} which should be displayed at
-         * {@code position}. Here we inflate a layout from the apps resources
-         * and then change the text view to signify the position.
-         */
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            // Inflate a new layout from our resources
-
-            View view = getLayoutInflater().inflate(R.layout.pager_item,
-                    container, false);
-            // Add the newly created View to the ViewPager
-            container.addView(view);
-
-            // Retrieve a TextView from the inflated View, and update it's text
-            TextView title = (TextView) view.findViewById(R.id.item_title);
-            title.setText(String.valueOf(position + 1));
-
-            // Return the View
-            return view;
-        }
-
-        /**
-         * Destroy the item from the {@link ViewPager}. In our case this is
-         * simply removing the {@link View}.
-         */
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
-        }
-
-    }
 }
--- app/src/main/java/org/arguman/app/ui/adapter/DashboardPagerAdapter.java
+++ app/src/main/java/org/arguman/app/ui/adapter/DashboardPagerAdapter.java
@@ -1,8 +1,67 @@
 package org.arguman.app.ui.adapter;
 
-public class DashboardPagerAdapter {
+import android.app.Activity;
+import android.content.res.Resources;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
+import org.arguman.app.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class DashboardPagerAdapter extends PagerAdapter {
+
+    // TODO: Define an argument list here.
+    private Activity activity;
+    private List<String> titleList;
+
+    public DashboardPagerAdapter(Activity activity) {
+        // TODO: Set argument list here.
+        this.activity = activity;
+        Resources resources = activity.getResources();
+        titleList = new ArrayList<>(
+                Arrays.asList(
+                        resources.getString(R.string.dashboard_tab_news_feed),
+                        resources.getString(R.string.dashboard_tab_recent),
+                        resources.getString(R.string.dashboard_tab_top),
+                        resources.getString(R.string.dashboard_tab_statistics)));
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return object == view;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titleList.get(position);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = activity.getLayoutInflater().inflate(R.layout.pager_item,
+                container, false);
+        container.addView(view);
+
+        ListView listView = (ListView) view.findViewById(R.id.pager_list_view);
+        // TODO: Fill listView with dynamic content.
+        // TODO: Implement an adapter for it.
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
 
 }
